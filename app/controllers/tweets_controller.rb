f932da239f7842ea812120e5c721a5d92b29f8da
ma2sh0505 <ma284sh05@gmail.com>
2020-11-23 12:41:07 +0900

    空白バリデーション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,8 @@
 class TweetsController < ApplicationController
-  
+  before_action :authenticate_user!,except:[:show]
+  before_action :set_tweet, only: [:show,:edit,:update,:destroy]
+
+
   def index
     @tweets = Tweet.all
   end
@@ -9,16 +12,19 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
+   
   end
 
   def create
-    Tweet.create(tweet_params)
-    redirect_to root_path
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else 
+      render :new
+    end
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
     if @tweet.destroy
     redirect_to root_path
     else
@@ -27,11 +33,10 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
+    
   end
 
   def update
-    @tweet = Tweet.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to root_path
     else
@@ -45,4 +50,7 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:title,:content,:start_time,:image).merge(user_id: current_user.id)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,9 @@ class Tweet < ApplicationRecord
   
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :start_time
+  end
 end
