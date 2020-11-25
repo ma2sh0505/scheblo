b84a1219716311fd6b7c6092c96902f2770cb8d7
ma2sh0505 <ma284sh05@gmail.com>
2020-11-25 12:34:29 +0900

    修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!,except:[:show]
+  before_action :authenticate_user!,except:[:index]
   before_action :set_tweet, only: [:show,:edit,:update,:destroy]
 
 
